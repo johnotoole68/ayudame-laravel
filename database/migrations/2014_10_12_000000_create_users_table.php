3a0e4cefbd30f0c80a1032543007d4adbd52e941
John O'Toole <john.otoole68@gmail.com>
2016-11-28 13:26:31 -0300

    correcciones de sintaxis
    corrijo algunos temas de la micgración

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreign('profiles_id')->references('id')->on('profiles');
+            $table->foreign('user_profiles_id')->references('id')->on('user_profiles');
             $table->foreign('addresses_id')->references('id')->on('addresses');
 
         });
