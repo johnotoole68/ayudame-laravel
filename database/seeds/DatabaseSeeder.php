65c3e0c01e2cec0b895f8eb893c02f851f609b20
John O'Toole <john.otoole68@gmail.com>
2016-12-06 22:07:24 -0300

    le pongo más onda al seeder...

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         factory(App\User::class, 50)->create()->each(function ($u) {
             $u->posts()->save(factory(App\Post::class)->make());
         });
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,8 +3,7 @@
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
-{
-    /**
+{    /**
      * Run the database seeds.
      *
      * @return void
@@ -18,8 +17,11 @@ class UsersTableSeeder extends Seeder
 
         //factory(App\User::class, 20)->create();
 
+
         factory(App\User::class, 100)->create()->each(function($user) {
-            factory(App\Perfil::class)->create(['user_profiles_id' => $user->id]);
+             factory(App\UserProfile::class)->create(['user_profiles_id' => $user->id]);
+             factory(App\Address::class)->create(['user_profiles_id' => $user->id]);
+
 
         // supposed to only apply to a single connection and reset it's self
         // but I like to explicitly undo what I've done for clarity
